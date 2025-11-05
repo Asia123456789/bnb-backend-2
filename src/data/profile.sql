9465fab80d3fc593c64c6de7835a75aa5affecf6
asia <asiia92@hotmail.com>
2025-11-05 20:31:24 +0100

    added user_id

--- src/data/profile.sql
+++ src/data/profile.sql
@@ -2,4 +2,5 @@ create table if not exists profiles (
   id uuid primary key default gen_random_uuid(),
   full_name text,
   is_admin boolean default false
+  user_id uuid unique -- viktigt för koppling till Supabase-auth
 );
